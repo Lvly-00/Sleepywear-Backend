d8e89c47fa68c1a78e212662dd9889b65e144c0f
LPintes03 <128210631+Lvly-00@users.noreply.github.com>
2025-09-19 22:25:31 +0800

    Added Orders & Invoices in api route

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\CollectionController;
 use App\Http\Controllers\UserSettingsController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
+
 require __DIR__ . '/auth.php';
 
 
@@ -23,4 +24,23 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user/settings', [UserSettingsController::class, 'show']);
     Route::put('/user/settings', [UserSettingsController::class, 'updateProfile']);
     Route::put('/user/settings/password', [UserSettingsController::class, 'updatePassword']);
+
+    // Orders & Invoices
+    Route::get('/orders', [OrderController::class, 'index']);
+    Route::post('/orders', [OrderController::class, 'store']); // used by add-order flow
+    Route::get('/orders/{order}', [OrderController::class, 'show']);
+    Route::put('/orders/{order}', [OrderController::class, 'update']);
+    Route::delete('/orders/{order}', [OrderController::class, 'destroy']);
+
+    // Order items listing
+    Route::get('/order-items', [OrderItemController::class, 'index']); // list all order items
+    Route::get('/order-items/{item}', [OrderItemController::class, 'show']);
+    Route::get('/order-items/{item}/customers', [OrderItemController::class, 'customers']);
+
+    // Invoices
+    Route::get('/invoices', [InvoiceController::class, 'index']);
+    Route::post('/invoices', [InvoiceController::class, 'store']); // create from add-order flow
+    Route::get('/invoices/{invoice}', [InvoiceController::class, 'show']);
+    Route::delete('/invoices/{invoice}', [InvoiceController::class, 'destroy']);
+    Route::get('/invoices/{invoice}/download', [InvoiceController::class, 'download']); // pdf
 });
