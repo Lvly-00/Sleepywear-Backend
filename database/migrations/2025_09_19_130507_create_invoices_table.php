906a2d62bd746761e1d76b430b61ac9d79f39054
LPintes03 <128210631+Lvly-00@users.noreply.github.com>
2025-09-19 21:09:35 +0800

    Create 2025_09_19_130507_create_invoices_table.php

---
+++ database/migrations/2025_09_19_130507_create_invoices_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->string('invoice_ref')->unique();
+            $table->data('issue_date')->nullable();
+            $table->data('sent_date')->nullable();
+            $table->string('customer_name')->nullable();
+            $table->string('status')->default('draft');
+            $table->text('notes')->nullable();
+            $table->decimal('total', 12, 2)->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
