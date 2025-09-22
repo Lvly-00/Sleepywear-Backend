f88e654aafe3c307700dc824e0b3b54c49a849ec
LPintes03 <128210631+Lvly-00@users.noreply.github.com>
2025-09-22 10:38:47 +0800

    added index function in invoiceController

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Invoice;
 use Illuminate\Http\Request;
 
 class InvoiceController extends Controller
 {
-    //
+    public function index()
+    {
+        $invoices = Invoice::with('orders.items')->orderBy('created_at', 'desc')->paginate(25);
+        return response()->json($invoices);
+    }
 }
