9a7160bb58de329248124aa5cf2066e20eacfd31
LPintes03 <128210631+Lvly-00@users.noreply.github.com>
2025-09-23 14:05:18 +0800

    added forgot password and erd installer

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'password-reset', 'forgot-password'],
     'allowed_methods' => ['*'],
     'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173')],
     'allowed_origins_patterns' => [],
