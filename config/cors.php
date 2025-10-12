d6f1ea8bd0c0694908820a0436a41aa923b46df4
Lovely <128210631+Lvly-00@users.noreply.github.com>
2025-10-12 16:14:43 +0800

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
     'allowed_methods' => ['*'],
     'allowed_origins' => [
         'http://localhost:5173',
-        'https://strong-roses-wish.loca.lt',
+        'https://sleepywear-frontend.onrender.com'
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
