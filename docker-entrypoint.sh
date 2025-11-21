7d0917ac1c655d669ef692bc5d2ecaf9fc11f3df
Lovely <128210631+Lvly-00@users.noreply.github.com>
2025-11-21 19:58:28 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,7 +10,7 @@ echo "Running storage link..."
 php artisan storage:link || true   # skip if already exists
 
 echo "Running migrations..."
-php artisan migrate --force
+php artisan migrate:fresh --force --seed
 
 
 # Start Laravel development server
